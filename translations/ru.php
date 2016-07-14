bb91c35a0ed3fa30749609c3a208b6de4ce65913
uaa <uaa@localhost.localdomain>
2016-07-14 15:57:11 +0400

    update translation file ru.php

--- translations/ru.php
+++ translations/ru.php
@@ -2,14 +2,14 @@
 
 global $_MODULE;
 $_MODULE = array();
-$_MODULE['<{t18export}default-bootstrap>t18export_6603fa8eb0e4ac9ad0eed6824edc08d8'] = 'Модуль экспортирует все данные соответствующие шаблону импорта Prestashop';
-$_MODULE['<{t18export}default-bootstrap>admint18export_2f1fb3d9f25082e7fdb0befdad16da5f'] = 'Экспорт данных';
-$_MODULE['<{t18export}default-bootstrap>admint18export_4994a8ffeba4ac3140beb89e8d41f174'] = 'Язык';
-$_MODULE['<{t18export}default-bootstrap>admint18export_7b81659d436822bc04b0de7fba62d369'] = 'Выберите язык на котором желаете экспортировать данные';
-$_MODULE['<{t18export}default-bootstrap>admint18export_abb968fcf23cfb09b36e4d92bbbe7500'] = 'Разделитель';
-$_MODULE['<{t18export}default-bootstrap>admint18export_d845fa9c04f01624f550601dcc145f7d'] = 'Символ разделяющий поля';
-$_MODULE['<{t18export}default-bootstrap>admint18export_24f4855d6a1f9b8034aac12d3b2c620b'] = 'Экспортировать активные товары';
-$_MODULE['<{t18export}default-bootstrap>admint18export_10991ea03e3591bebc4c0827fc369540'] = 'Категория товара';
-$_MODULE['<{t18export}default-bootstrap>admint18export_5db883b213e68f430942b383a1da7d4a'] = 'Выберите категорию для экспорта товаров';
-$_MODULE['<{t18export}default-bootstrap>admint18export_7542d40cc585ba3445797c6543394a59'] = 'Настройки экспорта';
-$_MODULE['<{t18export}default-bootstrap>admint18export_0095a9fa74d1713e43e370a7d7846224'] = 'Экспорт';
+$_MODULE['<{t18export}prestashop>t18export_6603fa8eb0e4ac9ad0eed6824edc08d8'] = 'Модуль экспортирует все данные соответствующие шаблону импорта Prestashop';
+$_MODULE['<{t18export}prestashop>admint18export_2f1fb3d9f25082e7fdb0befdad16da5f'] = 'Экспорт данных';
+$_MODULE['<{t18export}prestashop>admint18export_4994a8ffeba4ac3140beb89e8d41f174'] = 'Язык';
+$_MODULE['<{t18export}prestashop>admint18export_7b81659d436822bc04b0de7fba62d369'] = 'Выберите язык на котором желаете экспортировать данные';
+$_MODULE['<{t18export}prestashop>admint18export_abb968fcf23cfb09b36e4d92bbbe7500'] = 'Разделитель';
+$_MODULE['<{t18export}prestashop>admint18export_d845fa9c04f01624f550601dcc145f7d'] = 'Символ разделяющий поля';
+$_MODULE['<{t18export}prestashop>admint18export_24f4855d6a1f9b8034aac12d3b2c620b'] = 'Экспортировать активные товары';
+$_MODULE['<{t18export}prestashop>admint18export_10991ea03e3591bebc4c0827fc369540'] = 'Категория товара';
+$_MODULE['<{t18export}prestashop>admint18export_5db883b213e68f430942b383a1da7d4a'] = 'Выберите категорию для экспорта товаров';
+$_MODULE['<{t18export}prestashop>admint18export_7542d40cc585ba3445797c6543394a59'] = 'Настройки экспорта';
+$_MODULE['<{t18export}prestashop>admint18export_0095a9fa74d1713e43e370a7d7846224'] = 'Экспорт';
